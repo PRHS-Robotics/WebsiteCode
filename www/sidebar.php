a14ce5656dd54bea8ad9a71cc96b32969f6b720e
jasperhowald <jazdrum3r@yahoo.com>
2015-01-17 10:23:27 -0700

    Merge branch 'PHP' of https://github.com/PRHS-Robotics/WebsiteCode into PHP
    Conflicts:
    .htaccess
    www/sidebar.php

--- www/sidebar.php
+++ www/sidebar.php
@@ -8,9 +8,13 @@ if(!isset($home)){
 	<li><a href=<?PHP echo $home.'index.php';?>><h3>Home</h3></a></li>
 	<li><a href=<?PHP echo $home.'contact.php';?>><h3>Contact</h3></a></li>
 	<li><a href=<?PHP echo $home.'about/about.php';?>><h3>About</h3></a></li>
+<<<<<<< HEAD
     <li><a href=<?PHP echo $home.'sponsors.php';?>><h3>Sponsors</h3></a></li>
     <li><a href=<?PHP echo $home.'gallery.php';?>><h3>Gallery</h3></a></li>
 	<li><?PHP //include $home.'users/user.php';?></li>
+=======
+	<li><?PHP // echo '<hr>'; include $home.'users/user.php';?></li>
+>>>>>>> FETCH_HEAD
 </ul>
 </div>
 <?PHP
